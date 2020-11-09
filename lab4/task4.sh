8573061d1922e1b5f7d4d01aaa5457786147cd1c
Dkuriab <62207029+Dkuriab@users.noreply.github.com>
2020-11-09 15:46:41 +0300

    Update task4.sh

---
+++ lab4/task4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+ 
+last_backup=$(ls ~/ | grep Backup-)
+dest=~/restore/
+rm -rf $dest || true
+mkdir -p $dest
+ 
+if [ "$last_backup" == "" ]; then
+	echo "No backups found"
+	exit
+fi
+ 
+source=~/$last_backup
+ 
+for i in $(ls $source); do
+	without_suffix=""
+	if [[ "${#i}" -ge "12" ]]; then
+		without_suffix=${i::-11} || true
+	fi
+	if [[ "$without_suffix" == "" ]]; then
+		cp $source/$i $dest/$i
+	elif ! [[ -f $dest/$without_suffix ]]; then
+		cp $source/$i $dest/$i
+	fi
+done
